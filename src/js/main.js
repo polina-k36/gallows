68a229b45ea9f2893950afe36e6ed771876e4366
Polina Kazakova <websitework521@gmail.com>
2025-06-22 21:24:31 +0200

    functions project

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,50 @@
 //модалки
 
-const modalRule = document.querySelector('.overlay-rule'),
-      modalAnswer = document.querySelector('.overlay-answer'),
-      modalSignIn = document.querySelector('.overlay-signIn'),
+const modalRule = document.querySelector('.modal-rule'),
+      modalAnswer = document.querySelector('.modal-answer'),
+      modalSignIn = document.querySelector('.modal-signIn'),
       targetSignIn = document.querySelectorAll('[data-signin]'),
       targetAnswer = document.querySelectorAll('[data-answer]'),
       targetRule = document.querySelectorAll('[data-rule]'),
-      closeTargetModal = document.querySelectorAll('.modal__close');
+      targetPlay = document.querySelectorAll('[data-play]'),
+      closeTargetModal = document.querySelectorAll('.modal__close'),
+      overlay = document.querySelector('.overlay');
 
 
+//СДЕЛАТЬ РАБОТУ С МОДАЛЬНЫМИ ОКНАМИ БОЛЕЕ РАЗУМНО
+//показывание модалки
 function showModal(targetList, modal){
     targetList.forEach(item => {
         item.addEventListener('click', () => {
-            modal.style.display = 'block';
+            modal.style.display = 'flex';
+            overlay.style.display = 'block';
         })
     })
 }
 showModal(targetRule, modalRule);
-showModal(targetAnswer, modalAnswer);
+//showModal(targetAnswer, modalAnswer);
 showModal(targetSignIn, modalSignIn);
+
+//закрытие модалки через крест
 
 closeTargetModal.forEach(item => {
     item.addEventListener('click', () => {
         modalRule.style.display = 'none';
         modalSignIn.style.display = 'none';
         modalAnswer.style.display = 'none';
+        overlay.style.display = 'none';
     })
 });
+
+//закрытие через overlay
+
+document.addEventListener('click', e => {
+    if (e.target == overlay){
+        modalRule.style.display = 'none';
+        modalSignIn.style.display = 'none';
+        modalAnswer.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+})
+
 
---
+++ src/js/play.js
@@ -0,0 +1,178 @@
+//создание нужного количества букв для загаданного слова
+const words = {
+    all: [],
+    animals: [
+      'слон', 'тигр', 'лев', 'волк', 'медведь',
+      'заяц', 'лисица', 'еж', 'крокодил', 'обезьяна',
+      'жираф', 'пингвин', 'кит', 'орел', 'собака',
+      'кошка', 'зебра', 'бегемот', 'антилопа', 'кролик',
+      'панда', 'кенгуру', 'барсук', 'выдра', 'ласточка',
+      'сова', 'ястреб', 'кабан', 'морж', 'черепаха',
+      'дельфин', 'муравьед', 'страус', 'тигр', 'лисица',
+      'козёл', 'корова', 'осёл', 'бобр', 'попугай'
+    ],
+    fruits: [
+      'яблоко', 'банан', 'апельсин', 'груша', 'персик',
+      'слива', 'виноград', 'ананас', 'киви', 'арбуз',
+      'дыня', 'мандарин', 'кокос', 'черешня', 'лимон',
+      'гранат', 'финик', 'инжир', 'маракуйя', 'папайя',
+      'гуава', 'личи', 'черника', 'малина', 'клюква',
+      'смородина', 'абрикос', 'перец', 'томат', 'авокадо',
+      'дыня', 'карамбола', 'помело', 'шиповник', 'фейхоа',
+      'хурма', 'земляника', 'черешня', 'слива', 'грейпфрут'
+    ],
+    cities: [
+      'москва', 'париж', 'лондон', 'берлин', 'рим',
+      'киев', 'минск', 'стамбул', 'пекин', 'токио',
+      'мельбурн', 'каир', 'мумбаи', 'барселона',
+      'новосибирск', 'екатеринбург', 'казань', 'самара', 'омск',
+      'ростов', 'уфа', 'волгоград', 'пермь', 'краснодар',
+      'нижний', 'тольятти', 'челябинск', 'курск', 'воронеж',
+      'иваново', 'тамбов', 'орёл', 'липецк', 'брянск'
+    ],
+    professions: [
+      'врач', 'учитель', 'инженер', 'программист', 'повар',
+      'водитель', 'пилот', 'журналист', 'художник', 'певец',
+      'архитектор', 'полицейский', 'фермер', 'актёр', 'дизайнер',
+      'стоматолог', 'бухгалтер', 'менеджер', 'юрист', 'психолог',
+      'электрик', 'сантехник', 'строитель', 'парикмахер', 'фармацевт',
+      'переводчик', 'директор', 'оператор', 'кассир', 'продавец',
+      'водолаз', 'пожарный', 'солдат', 'моряк', 'плотник',
+      'садовник', 'таксист', 'курьер', 'бариста', 'логист'
+    ],
+    transport: [
+      'автомобиль', 'велосипед', 'самолёт', 'поезд', 'корабль',
+      'мотоцикл', 'трамвай', 'метро', 'автобус', 'вертолёт',
+      'катер', 'электросамокат', 'грузовик', 'скутер', 'яхта',
+      'пароход', 'троллейбус', 'монорельс', 'кабриолет', 'фургон',
+      'байк', 'джип', 'катамаран', 'пикап', 'рейсовый',
+      'скоростной', 'трамвайчик', 'водный', 'подводная', 'самокат',
+      'электромобиль', 'гидроцикл', 'погрузчик', 'экскаватор', 'бульдозер'
+    ],
+    colors: [
+      'красный', 'синий', 'зелёный', 'жёлтый', 'оранжевый',
+      'фиолетовый', 'чёрный', 'белый', 'коричневый', 'розовый',
+      'голубой', 'серый', 'бордовый', 'бирюзовый', 'лавандовый',
+      'малиновый', 'салатовый', 'пурпурный', 'золотой', 'серебряный',
+      'медный', 'бежевый', 'шоколадный', 'изумрудный', 'лазурный',
+      'песочный', 'терракотовый','хаки'
+      
+    ]
+  };
+  
+
+
+words.all = [
+...words.animals,
+...words.fruits,
+...words.cities,
+...words.professions,
+...words.transport,
+...words.colors
+];
+
+const modalSelectWords = document.querySelector('.modal-selectWords'),
+      overlay = document.querySelector('.overlay'),
+      checkboxes = modalSelectWords.querySelectorAll('input'),
+      allCheck = modalSelectWords.querySelector('#all'),
+      ownWordButton = document.querySelector('[data-inputWord]'),
+      startGameButton = document.querySelector('[data-startGame]');
+
+const letterWrapper = document.querySelector('.game__word');
+
+const keysKeyboard = document.querySelectorAll('.game__keyboard_key'),
+      modalGameOver = document.querySelector('.modal-gameOver'),
+      gallowsPart = [...document.querySelector('.game__gallows').children, 
+                     ...document.querySelector('.game__gallows-body').children];
+let attempts = 0; //количество попыток
+let unlockLetter = 0; //количество открытых букв
+
+let word;
+
+let selectWords = [];
+
+modalSelectWords.style.display = 'flex';
+overlay.style.display = 'block';
+
+allCheck.addEventListener('change', () => {
+    if (allCheck.checked){
+        checkboxes.forEach(check => {
+            check.checked = true;
+        });
+    } else {
+        checkboxes.forEach(check => {
+            check.checked = false;
+        });
+    }
+});
+
+startGameButton.addEventListener('click', () => {
+    checkboxes.forEach(check => {
+        if (check.checked){
+            selectWords = [...selectWords, ...words[check.id]]        
+        }
+    });
+    if (selectWords.length === 0){
+        alert('Выберите какую-нибудь категорию или придумайте свое слово для друга.');
+        return;
+    }
+    modalSelectWords.style.display = 'none';
+    overlay.style.display = 'none';
+    renderWord();
+    keyPress(letterWrapper.querySelectorAll('.game__word_letter'));
+})
+
+function renderWord(){
+    word = selectWords[Math.floor(Math.random()*selectWords.length)];
+    let letter;
+    for (let i = 0; i < word.length; i++) {
+        letter = document.createElement('div');
+        letter.classList.add('game__word_letter');
+        letter.textContent = word[i];
+        letterWrapper.append(letter);
+    }
+}
+
+//взаимодействие с клавиатурой
+
+function keyPress(letters) {
+    function onClickHandler() {
+        if (word.indexOf(this.textContent) !== -1) {
+            this.classList.add('game__keyboard_key-right');
+            letters.forEach(letter => {
+                if (letter.textContent === this.textContent) {
+                    letter.classList.add('game__word_letter-unlock');
+                    unlockLetter++;
+                }
+            });
+        } else {
+            this.classList.add('game__keyboard_key-wrong');
+            gallowsPart[attempts].style.display = 'block';
+            attempts++;
+        }
+        if (attempts >= 9) {
+            keysKeyboard.forEach(key => {
+                key.removeEventListener('click', onClickHandler);
+            });
+            setTimeout(() => {
+                modalGameOver.querySelector('.modal__result').textContent = 'Ты проиграл. Слово было - '+ word;
+                modalGameOver.style.display = 'flex';
+                overlay.style.display = 'block';
+            }, 1000)
+            return;
+        }
+        if (unlockLetter === word.length){
+            setTimeout(() => {
+                modalGameOver.querySelector('.modal__result').textContent = 'Ты выиграл!!! Слово действительно было - '+ word;
+                modalGameOver.style.display = 'flex';
+                overlay.style.display = 'block';
+            }, 1000)
+            return;
+        }
+    }
+
+    keysKeyboard.forEach(key => {
+        key.addEventListener('click', onClickHandler);
+    });
+}
+
