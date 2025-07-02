d40491f874485aa70c9e58b3f120475fa0e002c3
Polina Kazakova <websitework521@gmail.com>
2025-07-02 20:09:02 +0200

    start do animation move cat

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,8 @@ const modalRule = document.querySelector('.modal-rule'),
       targetRule = document.querySelectorAll('[data-rule]'),
       targetPlay = document.querySelectorAll('[data-play]'),
       closeTargetModal = document.querySelectorAll('.modal__close'),
-      overlay = document.querySelector('.overlay');
+      overlay = document.querySelector('.overlay'),
+      cat = document.querySelector('.cat');
 
 
 //СДЕЛАТЬ РАБОТУ С МОДАЛЬНЫМИ ОКНАМИ БОЛЕЕ РАЗУМНО
@@ -45,6 +46,46 @@ document.addEventListener('click', e => {
         modalAnswer.style.display = 'none';
         overlay.style.display = 'none';
     }
+});
+
+let bottomValue;
+let rightValue;
+let catMoveUpInterval;
+let catMoveDownInterval;
+cat.addEventListener('click', () => {
+    cat.classList.toggle('cat-sprite');
+    if (cat.classList.contains('cat-sprite')){
+        catMoveUpInterval = setInterval(() => {
+            bottomValue = parseInt(window.getComputedStyle(cat).bottom, 10) || 0;
+            rightValue = parseInt(window.getComputedStyle(cat).right, 10) || 0;
+            cat.style.transform = 'rotate(-50deg)';
+            if (bottomValue <= 450 && rightValue <= 1300) {
+                bottomValue = parseInt(window.getComputedStyle(cat).bottom, 10) || 0;
+                rightValue = parseInt(window.getComputedStyle(cat).right, 10) || 0;
+                cat.style.bottom = (bottomValue + 3) + 'px';
+                cat.style.right = (rightValue + 10) + 'px';
+            } else {
+                clearInterval(catMoveUpInterval);
+                catMoveDownInterval = setInterval(() => {
+                    bottomValue = parseInt(window.getComputedStyle(cat).bottom, 10) || 0;
+                    rightValue = parseInt(window.getComputedStyle(cat).right, 10) || 0;
+                    cat.style.transform = 'rotate(-250deg)';
+                    if (bottomValue >= 20 && rightValue >= 50) {
+                        bottomValue = parseInt(window.getComputedStyle(cat).bottom, 10) || 0;
+                        rightValue = parseInt(window.getComputedStyle(cat).right, 10) || 0;
+                        cat.style.bottom = (bottomValue - 3) + 'px';
+                        cat.style.right = (rightValue - 10) + 'px';
+                    } else {
+                        clearInterval(catMoveDownInterval);
+                        cat.style.transform = 'rotate(-10deg)';
+                        cat.classList.remove('cat-sprite');
+                    }
+                }, 50);
+            }
+        }, 50);
+    } else {
+        console.log('пушин не бежит');
+        clearInterval(catMoveUpInterval);
+        clearInterval(catMoveDownInterval);
+    }
 })
-
-
